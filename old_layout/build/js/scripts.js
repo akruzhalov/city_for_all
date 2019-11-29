b7d899bc40118d4e2016a86dc6da689d039c92d4
Alex Kruzhalov <alexkruzhalov@gmail.com>
2019-11-29 23:47:45 +0300

    [master] Add old_layout

---
+++ old_layout/build/js/scripts.js
@@ -0,0 +1,15 @@
+
+
+
+$(document).ready(function(){
+  $("#menu").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 1000);
+  });
+});
+
+
+
+
