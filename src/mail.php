1aa181f1bc9b8d04d1da12b6b421ea021550cc71
EkaterinaKornienko <kate_number_2000@mail.ru>
2020-05-29 03:12:51 +0600

    CFA-49, Add composer files, add directory mail_templates, update mail.php and mail2.php,add 'address' field to a taxi.html and taxislabo.html, changed the name of the field 'birthdate'-'birthday' in nana.php nanaslabo.php and mail2.php

--- src/mail.php
+++ src/mail.php
@@ -1,15 +1,48 @@
 <?php
-include("mail_template.php");
+require_once __DIR__ . '/vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
 
-$from = $_POST['from'];
+$mail = new PHPMailer;
+
+$mail->CharSet = 'utf-8';
+
+$fio = $_POST['fio'];
+$phone = $_POST['phone'];
+$address = $_POST['address'];
+$birthday = $_POST['birthday'];
 $select = $_POST['select'];
-
-
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-$mail = new PHPMailer;
-$mail-> Body = '<table style="border-collapse: collapse;" border="1"><tr><td> ФИО:</td><td>' .$fio. '</td></tr><tr><td>Телефон:</td><td>'  .$phone. '</td></tr><tr><td>Дата рождения:</td><td>' .$from. '</td></tr><tr><td>Относится к категории:</td><td>' .$select. '</td></tr></table>';
+$message = file_get_contents('mail_templates/mail.html');
+
+// Replace the % with the actual information
+$message = str_replace('%fio%', $fio, $message);
+$message = str_replace('%phone%', $phone, $message);
+$message = str_replace('%address%', $address, $message);
+$message = str_replace('%birthday%', $birthday, $message);
+$message = str_replace('%select%', $select, $message);
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'cityforall@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'abumed00'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('cityforall@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('cityforall@mail.ru');     // Кому будет уходить письмо
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заказ такси';                      // Тема письма
+$mail->MsgHTML($message);
 $mail->AltBody = '';
 
 if(!$mail->send()) {
@@ -17,4 +50,5 @@ if(!$mail->send()) {
 } else {
     header('location: thank-you.html');
 }
+?>
 
--- src/mail2.php
+++ src/mail2.php
@@ -1,21 +1,65 @@
-  <?php
-  include("mail_template.php");
 
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
 
-  $birthdate = $_POST['birthdate'];
-  $serial = $_POST['serial'];
-  $number = $_POST['number'];
-  $datapas = $_POST['datapas'];
-  $issuedby = $_POST['issuedby'];
-  $actualadress = $_POST['actualadress'];
-  $date = $_POST['date'];
-  $regadress = $_POST['regadress'];
+$mail = new PHPMailer;
 
-$mail-> Body = '<table style="border-collapse: collapse;" border="1"><tr><td> ФИО:</td><td>' .$fio. '</td></tr><tr><td>Телефон:</td><td>'  .$phone. '</td></tr><tr><td>Паспорт серия номер:</td><td>' .$serial .$number. '</td><tr/><tr><td>Дата выдачи:</td><td>' .$datapas. '</td></tr><tr><td>Кем выдан:</td><td>' .$issuedby.'</td></tr><tr><td>Адрес регистрации:</td><td>' .$regadress. '</td></tr><tr><td>Адрес фактического проживания:</td><td>' .$actualadress. '</td></tr><tr><td>Дата и время:</td><td>' .$date. '</td</tr></table>';
+$mail->CharSet = 'utf-8';
+
+$fio = $_POST['fio'];
+$phone = $_POST['phone'];
+$birthday = $_POST['birthday'];
+$serial = $_POST['serial'];
+$number = $_POST['number'];
+$datapas = $_POST['datapas'];
+$issuedby = $_POST['issuedby'];
+$actualadress = $_POST['actualadress'];
+$date = $_POST['date'];
+$regadress = $_POST['regadress'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$message = file_get_contents('mail_templates/mail2.html');
+
+// Replace the % with the actual information
+$message = str_replace('%fio%', $fio, $message);
+$message = str_replace('%phone%', $phone, $message);
+$message = str_replace('%birthday%', $birthday, $message);
+$message = str_replace('%serial%', $serial, $message);
+$message = str_replace('%number%', $number, $message);
+$message = str_replace('%datapas%', $datapas, $message);
+$message = str_replace('%issuedby%', $issuedby, $message);
+$message = str_replace('%actualadress%', $actualadress, $message);
+$message = str_replace('%date%', $date, $message);
+$message = str_replace('%regadress%', $regadress, $message);
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'cityforall@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'abumed00'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('cityforall@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('cityforall@mail.ru');     // Кому будет уходить письмо
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заказ няни';                       // Тема письма
+$mail->MsgHTML($message);
 $mail->AltBody = '';
+
 if(!$mail->send()) {
     echo 'Error';
 } else {
     header('location: thank-you.html');
 }
+?>
 
