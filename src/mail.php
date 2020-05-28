e31e437cc37e62b60e5d9a6ca55274c1c3ce46fc
EkaterinaKornienko <kate_number_2000@mail.ru>
2020-05-28 19:41:00 +0600

    CFA-44, Updated mail.php, mail2.php encoding fixed and AddReplyTo and addAddress added

--- src/mail.php
+++ src/mail.php
@@ -16,22 +16,26 @@ $phone = $_POST['phone'];
 $from = $_POST['from'];
 $select = $_POST['select'];
 
+$mail->CharSet = $config['CharSet'];                 // Кодировка обмена сообщениями с SMTP сервером
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = $config['Host'];  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = $config['SMTPAuth'];                                // Enable SMTP authentication
-$mail->Username = $config['Username'];  // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = $config['Password']; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = $config['SMTPSecure'];                           // Enable TLS encryption, `ssl` also accepted
-$mail->Port = $config['Port'];// TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->isSMTP();                                     // Set mailer to use SMTP
+$mail->Host = $config['Host'];  					 // Host SMTP сервера: ip или доменное имя
+$mail->SMTPAuth = $config['SMTPAuth'];               // Наличие авторизации на SMTP сервере
+$mail->Username = $config['Username'];               // Имя пользователя на SMTP сервере
+$mail->Password = $config['Password'];               // Пароль от учетной записи на SMTP сервере
+$mail->SMTPSecure = $config['SMTPSecure'];           // Тип шифрования
+$mail->Port = $config['Port'];                       // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom($config['Username']); // от кого будет уходить письмо?
-$mail->addAddress($config['Username']); // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Заказ такси';
+$mail->setFrom($config['Username']);                 // от кого будет уходить письмо? (Адресант почтового сообщения )
+$mail->addAddress($config['Username_to']);           // Получатель письма (Адресат почтового сообщения)
+//$mail->AddReplyTo($config['addreply']);            // Альтернативный адрес для ответа
+$mail->isHTML(true);                           // Set email format to HTML                                // Set email format to HTML
+
+$mail->Subject = 'Заказ такси';                      // Тема письма
 $mail-> Body = 'ФИО: '.$fio. '<br/> Телефон: '  .$phone. '<br/>Дата рождения: ' .$from. '<br/>Относится к категории: ' .$select;
 $mail->AltBody = '';
+
 if(!$mail->send()) {
     echo 'Error: ' . $mail->ErrorInfo;
 } else {
--- src/mail2.php
+++ src/mail2.php
@@ -24,24 +24,28 @@ $date = $_POST['date'];
 $regadress = $_POST['regadress'];
 
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+//$mail->SMTPDebug = 3;                              // Enable verbose debug output
+
+$mail->CharSet = $config['CharSet'];                 // Кодировка обмена сообщениями с SMTP сервером
+
+$mail->isSMTP();                                     // Set mailer to use SMTP
+$mail->Host = $config['Host'];  					 // Host SMTP сервера: ip или доменное имя
+$mail->SMTPAuth = $config['SMTPAuth'];               // Наличие авторизации на SMTP сервере
+$mail->Username = $config['Username'];               // Имя пользователя на SMTP сервере
+$mail->Password = $config['Password'];               // Пароль от учетной записи на SMTP сервере
+$mail->SMTPSecure = $config['SMTPSecure'];           // Тип шифрования
+$mail->Port = $config['Port'];                       // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = $config['Host'];  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = $config['SMTPAuth'];                                // Enable SMTP authentication
-$mail->Username = $config['Username'];  // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = $config['Password']; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = $config['SMTPSecure'];                           // Enable TLS encryption, `ssl` also accepted
-$mail->Port = $config['Port'];// TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->setFrom($config['Username']);                 // от кого будет уходить письмо? (Адресант почтового сообщения )
+$mail->addAddress($config['Username_to']);           // Получатель письма (Адресат почтового сообщения)
+//$mail->AddReplyTo($config['addreply']);            // Альтернативный адрес для ответа
+$mail->isHTML(true);                           // Set email format to HTML
 
-$mail->setFrom($config['Username']); // от кого будет уходить письмо?
-$mail->addAddress($config['Username']); // Optional name
-$mail->isHTML(true);                                   // Set email format to HTML
-
-$mail->Subject = 'Заказ няни';
+$mail->Subject = 'Заказ няни';                       // Тема письма
 $mail-> Body = 'ФИО: '.$fio. '<br/> Телефон: '  .$phone. '<br/>Паспорт серия номер: ' .$serial .$number. '<br/> Дата выдачи: ' .$datapas. '<br/> Кем выдан: ' .$issuedby.'<br/> Адрес регистрации: ' .$regadress. '<br/> Адрес фактического проживания: ' .$actualadress. '<br/> Дата и время: '.$date;
 $mail->AltBody = '';
+
 if(!$mail->send()) {
     echo 'Error: ' . $mail->ErrorInfo;
 } else {
