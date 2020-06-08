fc020e2c851d8718fbeb30c48a4af0ec35e579ac
EkaterinaKornienko <kate_number_2000@mail.ru>
2020-06-09 02:03:01 +0600

    removed repetition str_replace

--- src/mail.php
+++ src/mail.php
@@ -9,7 +9,7 @@ $yaml = new Parser();
 
 
 $config = $yaml->parse( file_get_contents('config.yml') )['smtp'];
-//var_dump($config);
+
 
 $fio = $_POST['fio'];
 $phone = $_POST['phone'];
@@ -17,28 +17,27 @@ $birthday = $_POST['birthday'];
 $address = $_POST['address'];
 $select = $_POST['select'];
 
+$info=[$fio,$phone,$birthday,$address,$select];
+$info_html=["%fio%","%phone%","%address%","%birthday%","%select%"];
+
 $mail->CharSet = $config['CharSet'];                 // Кодировка обмена сообщениями с SMTP сервером
 
 $message = file_get_contents('mail_templates/mail.html');
 
 // Replace the % with the actual information
-$message = str_replace('%fio%', $fio, $message);
-$message = str_replace('%phone%', $phone, $message);
-$message = str_replace('%address%', $address, $message);
-$message = str_replace('%birthday%', $birthday, $message);
-$message = str_replace('%select%', $select, $message);
+$message = str_replace($info_html, $info, $message);
 
 $mail->isSMTP();                                     // Set mailer to use SMTP
 $mail->Host = $config['Host'];  					 // Host SMTP сервера: ip или доменное имя
 $mail->SMTPAuth = $config['SMTPAuth'];               // Наличие авторизации на SMTP сервере
-$mail->Username = $config['Username'];               // Имя пользователя на SMTP сервере
+$mail->Username = $config['EmailFrom'];              // Имя пользователя на SMTP сервере
 $mail->Password = $config['Password'];               // Пароль от учетной записи на SMTP сервере
 $mail->SMTPSecure = $config['SMTPSecure'];           // Тип шифрования
 $mail->Port = $config['Port'];                       // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 
-$mail->setFrom($config['Username']);                 // от кого будет уходить письмо? (Адресант почтового сообщения )
-$mail->addAddress($config['Username_to']);           // Получатель письма (Адресат почтового сообщения)
+$mail->setFrom($config['EmailFrom']);                // от кого будет уходить письмо? (Адресант почтового сообщения )
+$mail->addAddress($config['EmailTo']);               // Получатель письма (Адресат почтового сообщения)
 //$mail->AddReplyTo($config['addreply']);            // Альтернативный адрес для ответа
 $mail->isHTML(true);                           // Set email format to HTML
 
--- src/mail2.php
+++ src/mail2.php
@@ -9,8 +9,6 @@ $yaml = new Parser();
 
 
 $config = $yaml->parse( file_get_contents('config.yml') )['smtp'];
-//var_dump($config);
-
 
 $fio = $_POST['fio'];
 $phone = $_POST['phone'];
@@ -23,35 +21,27 @@ $actualadress = $_POST['actualadress'];
 $date = $_POST['date'];
 $regadress = $_POST['regadress'];
 
-//$mail->SMTPDebug = 3;                              // Enable verbose debug output
+$info=[$fio,$phone,$birthday,$serial,$number,$datapas,$issuedby,$actualadress,$date,$regadress];
+$info_html=["%fio%","%phone%","%birthday%","%serial%","%number%","%datapas%","%issuedby%","%actualadress%","%date%","%regadress%"];
 
 $mail->CharSet = $config['CharSet'];                 // Кодировка обмена сообщениями с SMTP сервером
 
-$message = file_get_contents('mail_templates/mail2.html');
+$message = file_get_contents('mail_templates/mail.html');
 
 // Replace the % with the actual information
-$message = str_replace('%fio%', $fio, $message);
-$message = str_replace('%phone%', $phone, $message);
-$message = str_replace('%birthday%', $birthday, $message);
-$message = str_replace('%serial%', $serial, $message);
-$message = str_replace('%number%', $number, $message);
-$message = str_replace('%datapas%', $datapas, $message);
-$message = str_replace('%issuedby%', $issuedby, $message);
-$message = str_replace('%actualadress%', $actualadress, $message);
-$message = str_replace('%date%', $date, $message);
-$message = str_replace('%regadress%', $regadress, $message);
+$message = str_replace($info_html, $info, $message);
 
 $mail->isSMTP();                                     // Set mailer to use SMTP
 $mail->Host = $config['Host'];  					 // Host SMTP сервера: ip или доменное имя
 $mail->SMTPAuth = $config['SMTPAuth'];               // Наличие авторизации на SMTP сервере
-$mail->Username = $config['Username'];               // Имя пользователя на SMTP сервере
+$mail->Username = $config['EmailFrom'];              // Имя пользователя на SMTP сервере
 $mail->Password = $config['Password'];               // Пароль от учетной записи на SMTP сервере
 $mail->SMTPSecure = $config['SMTPSecure'];           // Тип шифрования
 $mail->Port = $config['Port'];                       // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 
-$mail->setFrom($config['Username']);                 // от кого будет уходить письмо? (Адресант почтового сообщения )
-$mail->addAddress($config['Username_to']);           // Получатель письма (Адресат почтового сообщения)
+$mail->setFrom($config['EmailFrom']);                // от кого будет уходить письмо? (Адресант почтового сообщения )
+$mail->addAddress($config['EmailTo']);               // Получатель письма (Адресат почтового сообщения)
 //$mail->AddReplyTo($config['addreply']);            // Альтернативный адрес для ответа
 $mail->isHTML(true);                           // Set email format to HTML
 
